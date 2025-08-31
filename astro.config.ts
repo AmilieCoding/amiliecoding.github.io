2c5f892bbc825a78e798774af30e18120fd99cd5
Amilie <amilie@allthingslinux.org>
2025-08-31 13:51:08 +0100

    t

--- astro.config.ts
+++ astro.config.ts
@@ -18,7 +18,8 @@ import netlify from '@astrojs/netlify'
 
 export default defineConfig({
   adapter: netlify(), // Set adapter for deployment, or set `linkCard` to `false` in `src/config.ts`
-  site: themeConfig.site.website,
+  site: 'https://amiliecoding.github.io',
+  base: '/amiliecoding.github.io',
   image: {
     service: {
       entrypoint: 'astro/assets/services/sharp',
