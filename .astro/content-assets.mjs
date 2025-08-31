6429549ea03fa129371f88f49f59c28d4d3ebff9
Amilie <amilie@allthingslinux.org>
2025-08-31 18:38:19 +0100

    New page!

--- .astro/content-assets.mjs
+++ .astro/content-assets.mjs
@@ -1,1 +1,5 @@
-export default new Map();
+
+import __ASTRO_IMAGE_IMPORT_1PNqd7 from "./_assets/gnome-desktop-best/desktop-gnome-2025-08-31.png?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Fgnome-is-the-best.md";
+import __ASTRO_IMAGE_IMPORT_209cut from "./_assets/gnome-desktop-best/appearance-settings.png?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Fgnome-is-the-best.md";
+export default new Map([["./_assets/gnome-desktop-best/desktop-gnome-2025-08-31.png?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Fgnome-is-the-best.md", __ASTRO_IMAGE_IMPORT_1PNqd7], ["./_assets/gnome-desktop-best/appearance-settings.png?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Fgnome-is-the-best.md", __ASTRO_IMAGE_IMPORT_209cut]]);
+		
--- src/content/posts/gnome-is-the-best-desktop-environmernt-objectively.md
+++
@@ -1,6 +0,0 @@
----
-title: GNOME is the best desktop environmernt (objectively).
-pubDate: '2025-08-31'
----
-
-GNOME, objectively is the best desktop environment. At least in my opinion.
